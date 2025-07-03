a4049e908591556df37cab7c99972485813989d0
Flavius Gorgan <gorgan.flavius@ibm.com>
2025-07-03 19:08:29 +0300

    changes in TestController

---
+++ spring_boot_tutorial/src/main/java/com/unitbv/spring_boot_tutorial/Aexposition/TestRestController.java
@@ -0,0 +1,23 @@
+package com.unitbv.spring_boot_tutorial.Aexposition;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestRestController {
+
+    @GetMapping("/ping")
+    public String pingMethod() {
+        return "Up and running_upd";
+    }
+
+    @GetMapping("/ping-body")
+    public ResponseEntity<String> pingRespEnt () {
+        return new ResponseEntity<>("Resp Entity: up an running!", HttpStatus.CREATED);
+    }
+
+}
